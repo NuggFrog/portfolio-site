aaa82677b558cc2f1312e4b6fd2dae01fe8e88fe
Aurelai Rickert <81323207+NuggFrog@users.noreply.github.com>
2023-11-10 20:56:02 +0000

    Edit Info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aurelai Rickert",
+  title: "Student Developer",
+  email: "aurelairickert@proton.me",
+  gitHub: "NuggFrog",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "NuggFrog",
+  youTube: "NuggFrog",
 };
 
 const primaryColor = "#4E567E";
